c4a6ad2705b1040298b03de9ee83f1f074ef31de
bioinfo <krastegar0@gmail.com>
2022-10-25 18:12:30 -0700

    Update: Linear and Log regression work with multiple datasets

--- src/midterm.py
+++ src/midterm.py
@@ -21,16 +21,17 @@ from sklearn.preprocessing import LabelEncoder
 class read_data:
     def __init__(
         self,
-        pathway="/home/bioinfo/Desktop/BDA_602/src/heart.csv",
-        response="Cholesterol",
+        response,
+        df,
+        predictors=None
     ):
-        self.pathway = pathway
         self.response = response
-        self.df = pd.read_csv(self.pathway, sep=",")
+        self.df = pd.DataFrame(df)
+        self.predictors = predictors
         self.columns = self.df.columns
 
     def dataframe(self):
-        return self.df
+        return self.df, print(self.df)
 
     def ChangeBinaryToBool(self):
 
@@ -39,9 +40,8 @@ class read_data:
             if len(unique) == 2:
                 if unique[0] == 0 and unique[1] == 1:
                     self.df[col] = self.df[col].astype(bool)
-                elif unique[0].lower() == "n" and unique[1].lower() == "y":
-                    # self.df[col] = self.df[col].upper()
-                    self.df[col] = self.df[col].map({"Y": 1, "N": 0})
+                elif unique[0] in ('n','N','no','No') and unique[1] in ('y','Y', 'yes', 'Yes'):
+                    self.df[col] = self.df[col].map({unique[1]: 1, unique[0]: 0})
                     self.df[col] = self.df[col].astype(bool)
                 else:
                     pass
@@ -63,22 +63,25 @@ class read_data:
 
         # Looping through each column and determing conditions
         # to put columns into respective groups
-        for index, column in enumerate(df.columns):
+        list_of_columns = list(df.columns)
+        for index, column in enumerate(list_of_columns): #df.columns is a list of columns
             data_type = df[column]
+            col_name = self.columns[index]
             if data_type.dtype != "bool":
-                if data_type.dtype == "float64" or data_type.dtype == "int64":
-                    cont_array.append(self.columns[index])
-                    # print(self.columns[index], " Continuous")
+                if data_type.dtype in ["float64","int64"]:
+                    cont_array.append(col_name)
+                    # print(col_name, " Continuous")
                     continue
-                elif data_type.dtype == "string" or data_type.dtype == "object":
-                    cat_array.append(self.columns[index])
-                    # print(self.columns[index], " Not Continuous or Bool")
+                elif data_type.dtype in ["string", "object", "category"]:
+                    cat_array.append(col_name)
+                    # print(col_name, " Not Continuous or Bool")
                     continue
                 else:  # Raise Error for unknown data types
-                    raise TypeError("Unknown Data Type")
+                    raise TypeError("Error in column classification: Unknown column dtype")
+                    # print(col_name, data_type.dtype)
             else:
-                bool_array.append(self.columns[index])
-                # print(self.columns[index], " Boolean")
+                bool_array.append(col_name)
+                # print(col_name, " Boolean")
                 continue
             # cat_array = cat_array + bool_array
 
@@ -106,10 +109,13 @@ class Cat_vs_Cont(read_data):
     This object is meant to plot all the types of Data such as:
     Categorical Response vs Categorical Predictor
     Categorical Response vs Continuous Predictor ....etc
-    It also takes the p-values of each continuous variable
+    It also takes the p-values of each continuous variable.
+
+    This Class is mean to be used in a for loop against individual predictors 
+    and response
     """
 
-    def __init__(self, continuous, categorical=None, boolean=None, *args, **kwargs):
+    def __init__(self, continuous=None, categorical=None, boolean=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.categorical = categorical
         self.continuous = continuous
@@ -119,16 +125,16 @@ class Cat_vs_Cont(read_data):
         fig_2 = go.Figure()
         fig_2.add_trace(
             go.Violin(
-                x=self.df[self.categorical],
+                x=self.df[self.response],
                 y=self.df[self.continuous],
-                name=f"{self.df[self.categorical].name} vs {self.df[self.continuous].name}",
+                name=f"{self.df[self.response].name} vs {self.df[self.continuous].name}",
                 box_visible=True,
                 meanline_visible=True,
             )
         )
         fig_2.update_layout(
-            title=f"{self.df[self.categorical].name} vs {self.df[self.continuous].name}",
-            xaxis_title=f"{self.df[self.categorical].name}",
+            title=f"{self.df[self.response].name} vs {self.df[self.continuous].name}",
+            xaxis_title=f"{self.df[self.response].name}",
             yaxis_title=f"{self.df[self.continuous].name}",
         )
         fig_2.show()
@@ -140,13 +146,13 @@ class Cat_vs_Cont(read_data):
 
     def contResponse_vs_catPredictor(self):
 
-        hist_data = [self.df[self.continuous]]
-        group_labels = [f"{self.df[self.continuous].name}"]  # name of the dataset
+        hist_data = [self.df[self.response]]
+        group_labels = [f"{self.df[self.response].name}"]  # name of the dataset
         fig = ff.create_distplot(hist_data, group_labels)
         fig.show()
 
         fig.write_html(
-            file=f"ViolinPlots pt 2: {self.df[self.continuous].name}",
+            file=f"ViolinPlots pt 2: {self.df[self.response].name}",
             include_plotlyjs="cdn",
         )
         return
@@ -155,20 +161,20 @@ class Cat_vs_Cont(read_data):
 
         # I am using self.continuous as a place holder for another categorical variaible
         conf_matrix = confusion_matrix(
-            self.df[self.categorical], self.df[self.continuous]
+            self.df[self.response], self.df[self.continuous]
         )
         fig_no_relationship = go.Figure(
             data=go.Heatmap(z=conf_matrix, zmin=0, zmax=conf_matrix.max())
         )
         fig_no_relationship.update_layout(
             title="Categorical Predictor by Categorical Response",
-            xaxis_title=f"{self.df[self.categorical].name}",
+            xaxis_title=f"{self.df[self.response].name}",
             yaxis_title=f"{self.df[self.continuous].name}",
         )
         fig_no_relationship.show()
 
         fig_no_relationship.write_html(
-            file=f"HeatMap: {self.df[self.continuous].name} vs {self.df[self.categorical].name} ",
+            file=f"HeatMap: {self.df[self.continuous].name} vs {self.df[self.response].name} ",
             include_plotlyjs="cdn",
         )
         return
@@ -176,7 +182,7 @@ class Cat_vs_Cont(read_data):
     def contResponse_vs_contPredictor(self):
 
         y = self.df[self.continuous]
-        column = self.df[self.categorical]  # This is just a place holder for continuous
+        column = self.df[self.response]  # This is just a place holder for continuous
         predictor = statsmodels.api.add_constant(column)
         linear_regression_model = statsmodels.api.OLS(y, predictor)
         linear_regression_model_fitted = linear_regression_model.fit()
@@ -186,12 +192,12 @@ class Cat_vs_Cont(read_data):
         p_value = "{:.6e}".format(linear_regression_model_fitted.pvalues[1])
 
         fig = px.scatter(
-            x=self.df[self.continuous], y=self.df[self.categorical], trendline="ols"
+            x=self.df[self.continuous], y=self.df[self.response], trendline="ols"
         )
         fig.update_layout(
             title=f"Variable: {self.df[self.continuous].name}: (t-value={t_value}) (p-value={p_value})",
             xaxis_title=f"{self.df[self.continuous].name}",
-            yaxis_title=f"{self.df[self.categorical].name}",
+            yaxis_title=f"{self.df[self.response].name}",
         )
         fig.show()
         fig.write_html(
@@ -204,11 +210,10 @@ class Cat_vs_Cont(read_data):
 
         # if response type is boolean do logistic regression
         df = self.ChangeBinaryToBool()
-        # df = self.dataframe()
-        df[self.boolean] = df[self.boolean].astype(
-            int
-        )  # this is absolute necessary for plotting logits
-        y = df[self.boolean]
+        bool_col = df[self.response]
+        # bool col is only 1's....logistic regression will break
+        # The way I fixed it was using map function in ChangeBinaryToBool 
+        y = bool_col
         column = df[self.continuous]
         predictor1 = statsmodels.api.add_constant(column)
         logistic_regression_model = statsmodels.api.Logit(y, predictor1)
@@ -218,12 +223,13 @@ class Cat_vs_Cont(read_data):
         t_value = round(logistic_regression_model_fitted.tvalues[0], 6)
         p_value = "{:.6e}".format(logistic_regression_model_fitted.pvalues[0])
 
-        # plotting (such a bitch to figure out)
-        fig = px.scatter(x=df[self.continuous], y=df[self.boolean], trendline="ols")
+        # plotting (such a bitch to figure out) ---- how the f did i figure this out
+        # the problem is with the y variable, it is in True/False, needed to change it to 0/1
+        fig = px.scatter(x=self.df['pclass'], y=df[self.response].astype(int), trendline="ols")
         fig.update_layout(
             title=f"Variable: {df[self.continuous].name}: (t-value={t_value}) (p-value={p_value})",
             xaxis_title=f"{df[self.continuous].name}",
-            yaxis_title=f"{df[self.boolean].name}",
+            yaxis_title=f"{df[self.response].name}",
         )
         fig.show()
 
@@ -307,7 +313,10 @@ class DiffMeanResponse(read_data):
         return mean_square_diff, weighted_square_diff
 
     def Mean_Squared_DF(self):
-
+        '''
+        Mean of Response calculations and plots. This class is to be used in 
+        a for loop with individual response and columns 
+        '''
         # Reading in df
         df = self.ChangeBinaryToBool()
 
@@ -443,7 +452,7 @@ class DiffMeanResponse(read_data):
         return
 
 
-def get_test_data_set(data_set_name: str = None):
+def get_test_data_set(data_set_name: str = None)-> (pd.DataFrame, List[str], str):
     """Function to load a few test data sets
 
     :param:
@@ -537,63 +546,84 @@ def get_test_data_set(data_set_name: str = None):
 
 
 def main():
-
-    # read in object and dataframe
-    object = read_data()
-
-    # -- testing DF w/ Mean Response
-    bins = DiffMeanResponse("ChestPainType")
-    msd = bins.Mean_Squared_DF()
-    print(msd)
-    plots = bins.plot_Mean_diff()
+    # Getting DF, predictors, and response 
+    df, predictors, response = get_test_data_set()
+    
+    # read in object
+    object = read_data(response= response, 
+                        df=df, 
+                        predictors=predictors)
+    object.dataframe()
 
     # split predictor columns into respective groups
     # and response variable category
-
     continuous, categorical, boolean = object.checkColIsContOrCat()
 
-    """
+    # Getting response type
+    response_VarGroup = object.get_col_type(response)
+
     # plotting continuous predictors with response
+    # Also grabbing pvalues and tvalues from continuous responses 
+
     stats_values = []
     if continuous is not None:
         for cont_pred in continuous:
             if response_VarGroup == "continuous":
-                test = Cat_vs_Cont(cont_pred, response).contResponse_vs_contPredictor()
+                test = Cat_vs_Cont(cont_pred, 
+                response= response, 
+                df=df, 
+                predictors=predictors).contResponse_vs_contPredictor()
                 stats_values.append(test)
+            
             elif response_VarGroup == "categorical":
-                test = Cat_vs_Cont(response, cont_pred).contResponse_vs_catPredictor()
+                test = Cat_vs_Cont(
+                cont_pred,response= response, 
+                df=df, 
+                predictors=predictors).contResponse_vs_catPredictor()
+            
             elif response_VarGroup == "boolean":
                 test = Cat_vs_Cont(
-                    continuous=cont_pred, boolean=response
-                ).BoolResponse_vs_ContPredictor()
+                    continuous=cont_pred, 
+                    boolean=response, 
+                    response= response, 
+                    df=df, 
+                    predictors=predictors).BoolResponse_vs_ContPredictor()
                 stats_values.append(test)
             else:
                 raise TypeError("invalid input...by me")
-    else:
-        pass
-
+    
+    elif categorical is not None:
     # Plotting categorical predictors with response
-    if categorical is not None:
         for cat_pred in categorical:
             if response_VarGroup == "continuous":
-                test = Cat_vs_Cont(response, cat_pred).contResponse_vs_catPredictor()
+                test = Cat_vs_Cont(categorical=cat_pred, 
+                response= response, 
+                df=df, 
+                predictors=predictors).contResponse_vs_catPredictor()
+            
             elif response_VarGroup == "categorical":
-                test = Cat_vs_Cont(response, cat_pred).catResponse_vs_catPredictor()
+                test = Cat_vs_Cont(categorical=cat_pred,
+                response= response, 
+                df=df, 
+                predictors=predictors).catResponse_vs_catPredictor()
+            
             else:
-                pass
+                raise AttributeError(f"{response} is not being plotted correctly, issue with class?")
     else:
         pass
+    
 
+    '''
     # RF regressor, obtaining feature importance
     machineLearning = RF_importance(continuous)
     feature_ranking = machineLearning.RF_regressor()
-    
+
     # combining regression statistics with RF feature importance
     stats_df = pd.DataFrame(stats_values)
     stats_df.columns=['Feature', 't-val', 'p-val']
     stats_df['Feature_Importance'] = np.array(feature_ranking['Feature_Importance']).tolist()
     print(stats_df)
-    """
+    '''
     return
 
 
